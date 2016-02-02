6b5809401801d1b325cc4e16dcb8dd788e211509
yowcow <yowcow@cpan.org>
2016-02-02 11:44:15 +0000

    sanitize

--- Build.pm
+++ Build.pm
@@ -5,7 +5,7 @@ use Shell::Command;
 
 class Build is Panda::Builder {
     method build($workdir) {
-        my $makefiledir = $workdir;
+        my $makefiledir = "$workdir/src";
         my $destdir = "$workdir/resources";
         mkpath $destdir;
         make($makefiledir, $destdir);
